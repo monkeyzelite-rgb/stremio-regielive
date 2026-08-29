970c26a64f7f2f9a17eafffb6a827a79f2ea3ff6
monkeyzelite-rgb <monkeyzelite@gmail.com>
2026-08-29 23:05:25 +0300

    Update addon.js

--- addon.js
+++ addon.js
@@ -4,8 +4,6 @@ const { searchRegieLive } = require("./lib/regielive");
 
 const builder = new addonBuilder(manifest);
 
-// URL dinamic pentru Render sau Local
-const APP_URL = "https://stremio-regielive.vercel.app";
 
 builder.defineSubtitlesHandler(async function(args) {
     const subs = await searchRegieLive(args.id, args.type);
